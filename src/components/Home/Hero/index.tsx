2b0f57961e36a33dabd2d997160d1f5f13aef9ed
Amir Gheybi <amir.962@yahoo.com>
2024-07-01 12:02:30 +0330

    complete text animations

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -2,32 +2,50 @@ import React from 'react'
 import heroImage from "@/public/images/hero/hero-big.png"
 import { WaveBg } from '@/src/assets/svgs'
 import Image from 'next/image'
+import TextAnimations from '../../UI/Animation/TextAnimation'
 const HeroSection = () => {
 
 
 
-    return (
-        <div
-        className='w-full h-screen flex justify-center items-center relative snap-center'
-      >
-        <WaveBg className="w-full h-[105%] absolute inset-0 text-primary" />
-        <div className="hero  w-[80%] h-[80%] glass rounded-box mt-[4vmax]">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <Image
-              src={heroImage} width={heroImage.width} height={heroImage.height} alt='heroImage'
-              className="w-[40%] aspect-square rounded-lg shadow-2xl object-cover" />
-            <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
-              <p className="py-6 w-[50%]">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                quasi. In deleniti eaque aut repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
-            </div>
+  return (
+    <div
+      className='w-full h-screen flex justify-center items-center relative snap-center'
+    >
+      <WaveBg className="w-full h-[105%] absolute inset-0 text-primary" />
+      <div className="hero  w-[80%] h-[80%] glass rounded-box mt-[4vmax]">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <Image
+            src={heroImage} width={heroImage.width} height={heroImage.height} alt='heroImage'
+            className="w-[40%] aspect-square rounded-lg object-cover" />
+          <div>
+            <TextAnimations
+              text='Box Office News!'
+              boxClass='text-5xl font-bold'
+              el='h1'
+              staggerTime={0.1}
+              duration={0.1}
+              typeAnimation='typing'
+              once
+            />
+            <TextAnimations
+              text={[
+                'Provident cupiditate voluptatem et in. ',
+                "Quaerat fugiat ut assumenda excepturi ",
+                "exercitationem   quasi. In deleniti ",
+                "eaque aut repudiandae et a id nisi. "
+              ]}
+              boxClass='py-6 w-[50%]'
+              staggerTime={0.1}
+              duration={1}
+              delay={1.5}
+              once
+              typeAnimation='lineOpacity'
+            />
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default HeroSection
---
+++ src/components/UI/Animation/TextAnimation.tsx
@@ -0,0 +1,76 @@
+"use client"
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react"
+
+export interface TypingAnimationProps {
+    text: string | string[]
+    boxClass: string
+    staggerTime?: number
+    delay?: number
+    el?: keyof JSX.IntrinsicElements
+    duration?: number
+    amountView?: number,
+    once?: boolean
+    typeAnimation: "typing" | "lineOpacity"
+}
+
+const TextAnimations = ({ text, boxClass, delay, staggerTime, el: Wrapper = "p", duration, amountView, once, typeAnimation }: TypingAnimationProps) => {
+    const textArray: string[] = Array.isArray(text) ? text : [text]
+    const boxRef = useRef(null)
+    const isInView = useInView(boxRef, { amount: amountView ?? 0.5, once })
+    const typingVariant = {
+        initial: {
+            opacity: 0,
+            y: 20
+        },
+        animate: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: duration ?? 0
+            }
+        },
+
+    }
+
+
+    return (
+        <Wrapper className={boxClass}>
+            <span className="sr-only">{text}</span>
+            <motion.span
+                ref={boxRef}
+                initial="initial"
+                animate={isInView ? "animate" : "initial"}
+                transition={{ staggerChildren: staggerTime ?? 0.05, delayChildren: delay ?? 0 }}
+                aria-hidden
+            >
+
+                {textArray.map(line =>
+
+                    <span className="inline-block">
+                        {typeAnimation === "typing" ?
+
+                            line.split(" ").map(word =>
+                                <span className="inline-block">
+                                    {word.split("").map(char =>
+                                        <motion.span className="inline-block" variants={typingVariant}>{char}</motion.span>
+                                    )}
+                                    <span>&nbsp;</span>
+                                </span>
+                            )
+
+                            :
+                            <motion.span className="inline-block" variants={typingVariant}>
+                                {line}
+                            </motion.span>
+                        }
+
+                    </span>
+                )}
+
+            </motion.span>
+        </Wrapper>
+    )
+}
+
+export default TextAnimations
