4cbf9864eeed3fab200dc49c3e77b7c235e7dc3b
Amir Gheybi <amir.962@yahoo.com>
2024-07-14 16:47:10 +0330

    add loading page

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingUi from '@/src/components/UI/Loading'
+import React from 'react'
+
+const loading = () => {
+    return (
+        <LoadingUi isLoading/>
+    )
+}
+
+export default loading
