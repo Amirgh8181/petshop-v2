6694ae402aa35fbba72e77df8d596b03ab15731c
Amir Gheybi <amir.962@yahoo.com>
2024-07-19 14:27:11 +0330

    complete footer and profile messages

--- src/components/UI/NavBarSections/AuthSection/Profile.tsx
+++ src/components/UI/NavBarSections/AuthSection/Profile.tsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 import { FaHeart } from 'react-icons/fa'
 import { signOut } from 'next-auth/react'
 import { authSectionProps } from '.'
+import { useTranslations } from 'next-intl'
 
 const Profile = ({ isAuth }: { isAuth: authSectionProps }) => {
+    const t=useTranslations("Profile")
     return (
         <>
             {isAuth &&
@@ -23,19 +25,19 @@ const Profile = ({ isAuth }: { isAuth: authSectionProps }) => {
                         <li>
                             <Link href={'/FavoriteList'} className="flex items-center space-x-2">
                                 <span className="text-2xl"> <FaHeart /> </span>
-                                <span> Favorite List</span>
+                                <span>{t("favorite")}</span>
                             </Link>
                         </li>
                         <li>
                             <Link href={'/Cart'} className="flex items-center space-x-2">
                                 <span className="text-2xl"> <MdOutlineShoppingCart /> </span>
-                                <span> Shopping Cart</span>
+                                <span>{t("cart")}</span>
                             </Link>
                         </li>
                         <li>
                             <div className="flex items-center space-x-2" onClick={() => signOut()}>
                                 <span className="text-2xl"><BiLogIn /></span>
-                                <span className="font-bold"> Log Out</span>
+                                <span className="font-bold">{t("logout")}</span>
                             </div>
                         </li>
                     </ul>
